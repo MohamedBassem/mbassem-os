e9bf7adafe18bbfe3e11aa83605c1740463f6c5d
MohamedBassem <medoox240@gmail.com>
2018-03-03 18:53:56 +0000

    04 - The one about stacks

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,15 +1,32 @@
 [org 0x7c00]
 
-loop:
-	mov ah, 0x0e ; tty mode
 
-	mov al, [the_secret]
-	int 0x10
+mov ah, 0x0e ; tty mode
 
-	jmp $ ; Keep printing in an infinite loop
 
-the_secret:
-	db 'X'
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
+int 0x10
+
+
+pop bx
+mov al, bl
+int 0x10
+
+
+pop bx
+mov al, bl
+int 0x10
+
+jmp $ ; Keep printing in an infinite loop
+
 
 ; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
