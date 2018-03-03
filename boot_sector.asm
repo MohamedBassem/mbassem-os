4893c4c372c8ddb3ef7862b897502b3c76bde575
MohamedBassem <medoox240@gmail.com>
2018-03-03 18:44:00 +0000

    03 - I found the secret

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,17 +1,15 @@
-; Infinite loop (e9 fd ff)
+[org 0x7c00]
+
 loop:
 	mov ah, 0x0e ; tty mode
-	mov al, 'H'
+
+	mov al, [the_secret]
 	int 0x10
-	mov al, 'e'
-	int 0x10
-	mov al, 'l'
-	int 0x10
-	mov al, 'l'
-	int 0x10
-	mov al, 'o'
-	int 0x10
-	jmp loop ; Keep printing in an infinite loop
+
+	jmp $ ; Keep printing in an infinite loop
+
+the_secret:
+	db 'X'
 
 ; Fill with 510 zeros minus the size of the previous code
 times 510-($-$$) db 0
